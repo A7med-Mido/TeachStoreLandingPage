9f88d87019306c05831b7437c3099b12372de929
A7med-Mido <ahmedashrafodi.4@gmail.com>
2025-09-10 06:03:37 +0300

    init landing page repo

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Waves from './components/SVG'
+
+function App() {
+
+  return (
+    <div className='h-screen w-full flex justify-center items-center'>
+      <Waves className="w-full h-screen " />
+    </div>
+  )
+}
+
+export default App
